24a561b7d1312888cabd773309eb41dd11ca0ec0
apollo <apollo.ren@tuya.com>
2022-03-20 00:31:07 +0800

    mminimap0.1

--- routes/index.ts
+++ routes/index.ts
@@ -1,15 +1,15 @@
-// import About from './test/about'
-// import Home from './test/home'
+import About from './test/about'
+import Home from './test/home'
 
-// const MyRoutes = [...About, ...Home]
-// export default MyRoutes
+const MyRoutes = [...About, ...Home]
+export default MyRoutes
 
 // goodlive
 
-import HomePage from './homepage'
-import LifeServer from './lifeserver'
-import Shop from './shop'
-import User from './user'
-const IOuters = [...HomePage, ...LifeServer, ...Shop, ...User]
+// import HomePage from './homepage'
+// import LifeServer from './lifeserver'
+// import Shop from './shop'
+// import User from './user'
+// const IOuters = [...HomePage, ...LifeServer, ...Shop, ...User]
 
-export default IOuters
+// export default IOuters
--- src/pages/test/about/index.tsx
+++ src/pages/test/about/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { logData, logData2 } from '@/pages/constant'
+import { Button } from 'antd'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import './index.less'
+import MiniMap from './miniMap'
 const About = () => {
+  const [logDataa, setLogData] = useState<any>()
+  const logRef = useRef<any>(null)
+  const handleClick = () => {
+    console.log(logRef)
+  }
+  useEffect(() => {
+    setTimeout(() => setLogData(logData2), 10)
+  }, [])
   return (
     <>
       <div>
@@ -15,6 +26,22 @@ const About = () => {
         </ul>
         <hr />
         <h1>About</h1>
+        <div
+          style={{
+            fontSize: '18px',
+            color: 'red',
+            backgroundColor: 'skyBlue',
+            textAlign: 'center',
+          }}
+        >
+          字体大小能继承，颜色可以，背景颜色也可以<div>1</div>
+        </div>
+        <Button onClick={handleClick}>打印</Button>
+        <Button>1</Button>
+        <div id="about-box" style={{ position: 'relative', maxWidth: '1400px' }}>
+          <div className="about" dangerouslySetInnerHTML={{ __html: logDataa }} ref={logRef}></div>
+          <MiniMap content={logDataa} logNode={logRef.current} />
+        </div>
       </div>
     </>
   )
